5cdf5d03db21a8e482005c91e940d8c31244a923
whoami <az.khabibullin2018@gmail.com>
2024-06-09 01:07:45 +0500

    Добавил элементы tableLayoutPanel, dataGridView, Button, TextBox в tabPage для select'a добавил скрипт, который обрабатывает наш запрос через классы SqlDataAdapter и DataSet

--- learn_mssql_csharp/Form1.cs
+++ learn_mssql_csharp/Form1.cs
@@ -9,7 +9,6 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 
 using System.Configuration;
-using System.Data;
 using System.Data.SqlClient;
 
 namespace learn_mssql_csharp
@@ -17,6 +16,8 @@ namespace learn_mssql_csharp
     public partial class Form1 : Form
     {
         private SqlConnection sqlConnection = null;
+
+        //т.к таблицы слились, новое подключение не нужно
 
         public Form1()
         {
@@ -28,22 +29,11 @@ namespace learn_mssql_csharp
             sqlConnection = new SqlConnection(ConfigurationManager.ConnectionStrings["TestDB"].ConnectionString);
 
             sqlConnection.Open();
-
-            if(sqlConnection.State == ConnectionState.Open)
-            {
-                MessageBox.Show("Server connected"); 
-            }
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
             SqlCommand sqlCommand = new SqlCommand(
-                //"INSERT INTO [Students] (Name, Surname, Birthday, Birthplace, Phone, Email)" +
-                //"VALUES (N'Алекс', N'Алексеев', '02/02/2002', N'Москва', N'1', N'а')",
-
-                //"INSERT INTO [Students] (Name, Surname, Birthday, Birthplace, Phone, Email)" +
-                //$"VALUES (N'{textBox1.Text}', N'{textBox2.Text}', '{textBox3.Text}', N'{textBox4.Text}', N'{textBox5.Text}', N'{textBox6.Text}')",
-
                 "INSERT INTO [Students] (Name, Surname, Birthday, Birthplace, Phone, Email)" +
                 "VALUES (@Name, @Surname, @Birthday, @Birthplace, @Phone, @Email)",
                 sqlConnection
@@ -59,6 +49,23 @@ namespace learn_mssql_csharp
             sqlCommand.Parameters.AddWithValue("Email", textBox6.Text);
 
             MessageBox.Show(sqlCommand.ExecuteNonQuery().ToString());
+        }
+
+        private void tableLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //если писать запрос через конкатенацию, а не слитно, но скрипт не работает почему-то
+            //SELECT * FROM Products WHERE UnitPrice > 100
+            SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(
+                textBox7.Text,
+                sqlConnection);
+            DataSet dataSet = new DataSet();
+            sqlDataAdapter.Fill(dataSet);
+            dataGridView1.DataSource = dataSet.Tables[0];
         }
     }
 }
