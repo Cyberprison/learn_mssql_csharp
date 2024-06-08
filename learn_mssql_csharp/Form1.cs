7ddbde12e64a29c02bf941d943853ab81c5af850
whoami <az.khabibullin2018@gmail.com>
2024-06-08 14:22:51 +0500

    Добавил элемент tabPage, textBox, Button добавил событие на нажатие кнопки, добавление ввденной записи

--- learn_mssql_csharp/Form1.cs
+++ learn_mssql_csharp/Form1.cs
@@ -34,5 +34,31 @@ namespace learn_mssql_csharp
                 MessageBox.Show("Server connected"); 
             }
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SqlCommand sqlCommand = new SqlCommand(
+                //"INSERT INTO [Students] (Name, Surname, Birthday, Birthplace, Phone, Email)" +
+                //"VALUES (N'Алекс', N'Алексеев', '02/02/2002', N'Москва', N'1', N'а')",
+
+                //"INSERT INTO [Students] (Name, Surname, Birthday, Birthplace, Phone, Email)" +
+                //$"VALUES (N'{textBox1.Text}', N'{textBox2.Text}', '{textBox3.Text}', N'{textBox4.Text}', N'{textBox5.Text}', N'{textBox6.Text}')",
+
+                "INSERT INTO [Students] (Name, Surname, Birthday, Birthplace, Phone, Email)" +
+                "VALUES (@Name, @Surname, @Birthday, @Birthplace, @Phone, @Email)",
+                sqlConnection
+                );
+
+            DateTime dateTime = DateTime.Parse(textBox3.Text);
+
+            sqlCommand.Parameters.AddWithValue("Name", textBox1.Text);
+            sqlCommand.Parameters.AddWithValue("Surname", textBox2.Text);
+            sqlCommand.Parameters.AddWithValue("Birthday", $"{dateTime.Month}/{dateTime.Day}/{dateTime.Year}");
+            sqlCommand.Parameters.AddWithValue("Birthplace", textBox4.Text);
+            sqlCommand.Parameters.AddWithValue("Phone", textBox5.Text);
+            sqlCommand.Parameters.AddWithValue("Email", textBox6.Text);
+
+            MessageBox.Show(sqlCommand.ExecuteNonQuery().ToString());
+        }
     }
 }
